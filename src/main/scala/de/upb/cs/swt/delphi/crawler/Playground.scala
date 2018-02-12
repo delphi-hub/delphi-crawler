dc845e4439c68419f18a3ce164ab26d7f0bb43a3
Ben Hermann <ben.hermann@googlemail.com>
2018-02-12 14:59:35 +0100

    Added some raw crawler code in pursuit to implement #2. However, this is not ideal. It fills the pool to quickly and there is no very cool way to throttle this... I am trying to reimplement this with streams now.

--- src/main/scala/de/upb/cs/swt/delphi/crawler/Playground.scala
+++ src/main/scala/de/upb/cs/swt/delphi/crawler/Playground.scala
@@ -1,13 +1,12 @@
 package de.upb.cs.swt.delphi.crawler
 
-import java.net.URL
-
 import akka.actor.{ActorRef, ActorSystem}
+import akka.http.scaladsl.model.Uri
 import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.http.HttpClient
 import de.upb.cs.swt.delphi.crawler.elastic.ElasticActor
 import de.upb.cs.swt.delphi.crawler.maven.MavenCrawlActor
-import de.upb.cs.swt.delphi.crawler.maven.MavenCrawlActor.Discover
+import de.upb.cs.swt.delphi.crawler.maven.MavenCrawlActor.StartDiscover
 
 /**
   * Created by benhermann on 06.02.18.
@@ -19,7 +18,7 @@ object Playground extends App {
   val client = HttpClient(ElasticsearchClientUri("localhost", 9200))
   val elastic : ActorRef = system.actorOf(ElasticActor.props(client), "elastic")
 
-  val maven : ActorRef = system.actorOf(MavenCrawlActor.props(new URL("http://search.maven.org"), elastic), "maven")
+  val maven : ActorRef = system.actorOf(MavenCrawlActor.props(Uri("https://repo.maven.apache.org/maven2/de/tu-darmstadt/"), elastic), "maven")
 
-  maven ! Discover
+  maven ! StartDiscover
 }
--- src/main/scala/de/upb/cs/swt/delphi/crawler/maven/MavenCrawlActor.scala
+++ src/main/scala/de/upb/cs/swt/delphi/crawler/maven/MavenCrawlActor.scala
@@ -3,18 +3,93 @@ package de.upb.cs.swt.delphi.crawler.maven
 import java.net.URL
 
 import akka.actor.{Actor, ActorLogging, ActorRef, Props}
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.Uri.Path
+import akka.http.scaladsl.model.{HttpMethod, HttpMethods, HttpRequest, Uri}
+import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
+import akka.util.ByteString
+import coursier.core.compatibility
+import coursier.{Cache, Fetch}
+import coursier.maven.{MavenRepository, Pom}
+import coursier.util.WebPage
+import de.upb.cs.swt.delphi.crawler.BlockingHttpClient
 import de.upb.cs.swt.delphi.crawler.elastic.ElasticActor.Push
-import de.upb.cs.swt.delphi.crawler.maven.MavenCrawlActor.{Discover, ProcessNew}
+import de.upb.cs.swt.delphi.crawler.maven.MavenCrawlActor.{DiscoverMetadata, ProcessMetadata, ProcessNew, StartDiscover}
+
+import scala.util.{Failure, Success}
+import scalaz.{-\/, \/, \/-}
 
 
 /**
   * Created by benhermann on 05.02.18.
   */
-class MavenCrawlActor(baseURL : URL, elasticActor : ActorRef) extends Actor with ActorLogging {
+class MavenCrawlActor(baseUri : Uri, elasticActor : ActorRef) extends Actor with ActorLogging {
+  implicit val executionContext = context.system.dispatcher
+  implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(context.system))
+  val http = Http(context.system)
+
+
   override def receive = {
-    case Discover => {
-      log.info("Discovery run for baseURL {}", baseURL)
-      self ! ProcessNew(new MavenIdentifier("g", "a", "v"))
+    case StartDiscover => {
+      log.info("Discovery run for baseURL {}", baseUri)
+
+      // Poll and parse last_updated.txt
+      val req = http.singleRequest(HttpRequest(method = HttpMethods.HEAD, uri = baseUri.withPath(baseUri.path + "last_updated.txt")))
+
+      req.onComplete {
+        case Success(res) => {
+          log.info(res.toString())
+          log.info(res.getHeader("Last-Modified").get().value())
+          res.entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach { body =>
+            log.info("Got response, body: " + body.utf8String)
+          }
+          self ! DiscoverMetadata(baseUri)
+        }
+        case Failure(_) => log.error("Oh no!")
+      }
+
+    }
+    case DiscoverMetadata(currentUri : Uri) => {
+      log.debug(s"Crawling for metadata file in: $currentUri")
+
+      val req = http.singleRequest(HttpRequest(uri = currentUri))
+
+      req.onComplete {
+        case Success(res) => {
+          res.entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach { body =>
+            val files = WebPage.listFiles(currentUri.toString(), body.utf8String)
+            if (files.contains("maven-metadata.xml")) {
+              self ! ProcessMetadata(currentUri.withPath(currentUri.path + "maven-metadata.xml"))
+            } else {
+              val directories = WebPage.listDirectories(currentUri.toString(), body.utf8String)
+              directories.foreach(s => self ! DiscoverMetadata(currentUri.withPath(currentUri.path + s + "/")))
+
+            }
+          }
+        }
+        case Failure(e) => log.warning(s"Failure for $currentUri - $e" )
+      }
+    }
+    case ProcessMetadata(metadataUri : Uri) => {
+      log.debug(s"Processing metadata at $metadataUri")
+
+      val req = http.singleRequest(HttpRequest(uri = metadataUri))
+
+      req.onComplete {
+        case Success(res) => {
+          res.entity.dataBytes.runFold(ByteString(""))(_ ++ _).foreach { body =>
+            var v = for {
+              xml <- \/.fromEither(coursier.core.compatibility.xmlParse(body.utf8String))
+              _ <- if (xml.label == "metadata") \/-(()) else -\/("Metadata not found")
+              versions <- Pom.versions(xml)
+            } yield versions
+
+            v.foreach(v => log.info(s"Found latest version: ${v.latest} for ${metadataUri}"))
+
+          }
+        }
+        case Failure(_) => log.error("Metadata get failed.")
+      }
     }
     case ProcessNew(m : MavenIdentifier) => {
       elasticActor ! Push(m)
@@ -22,13 +97,29 @@ class MavenCrawlActor(baseURL : URL, elasticActor : ActorRef) extends Actor with
       // retrieve binary
       // push to hermes
     }
+
+      /*
+      val repositories = Seq(
+        MavenRepository(baseUri.toString())
+      )
+
+
+      val fetch = Fetch.from(repositories, Cache.fetch())
+
+
+
+      val stuff = BlockingHttpClient.doGet(context.system, Uri(repositories.head.root0))
+      var result = WebPage.listDirectories(repositories.head.root0, stuff)
+      */
   }
 }
 
 object MavenCrawlActor {
-  def props(baseURL : URL, elasticActor : ActorRef) : Props = Props(new MavenCrawlActor(baseURL, elasticActor))
+  def props(baseURL : Uri, elasticActor : ActorRef) : Props = Props(new MavenCrawlActor(baseURL, elasticActor))
 
-  case object Discover
+  case object StartDiscover
+  case class DiscoverMetadata(currentUri : Uri)
+  case class ProcessMetadata(metadataUri : Uri)
   case class ProcessNew(identifier : MavenIdentifier)
 
 }
