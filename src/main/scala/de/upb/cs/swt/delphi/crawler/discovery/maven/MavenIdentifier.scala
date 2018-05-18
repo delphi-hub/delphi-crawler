545803536c97aaa49518d40bebfc068759bf8f15
Ben Hermann <ben.hermann@googlemail.com>
2018-05-18 19:17:36 +0200

    Added repository to Maven identifier

--- src/main/scala/de/upb/cs/swt/delphi/crawler/discovery/maven/MavenIdentifier.scala
+++ src/main/scala/de/upb/cs/swt/delphi/crawler/discovery/maven/MavenIdentifier.scala
@@ -2,7 +2,4 @@ package de.upb.cs.swt.delphi.crawler.discovery.maven
 
 import de.upb.cs.swt.delphi.crawler.Identifier
 
-/**
-  * Created by benhermann on 06.02.18.
-  */
-class MavenIdentifier(val groupId: String, val artifactId: String, val version: String) extends Identifier
+case class MavenIdentifier(val repository: String, val groupId: String, val artifactId: String, val version: String) extends Identifier
