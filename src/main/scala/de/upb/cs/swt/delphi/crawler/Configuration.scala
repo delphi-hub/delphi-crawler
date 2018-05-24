70ae83e42f0425773a59eb0ae2788d44fa2669e8
Ben Hermann <ben.hermann@googlemail.com>
2018-05-24 13:49:37 +0200

    Made the elasticsearch URI configurable

--- src/main/scala/de/upb/cs/swt/delphi/crawler/Configuration.scala
+++ src/main/scala/de/upb/cs/swt/delphi/crawler/Configuration.scala
@@ -4,8 +4,10 @@ import java.net.URI
 import com.sksamuel.elastic4s.ElasticsearchClientUri
 
 
-class Configuration(val elasticsearchClientUri: ElasticsearchClientUri = ElasticsearchClientUri("localhost", 9200),
+class Configuration(val elasticsearchClientUri: ElasticsearchClientUri = ElasticsearchClientUri(sys.env.getOrElse("DELPHI_ELASTIC_URI","elasticsearch://localhost:9200")),
                     val mavenRepoBase: URI = new URI("http://localhost:8881/maven2/"),
                     val controlServerPort : Int = 8882)  {
 
+
 }
+
