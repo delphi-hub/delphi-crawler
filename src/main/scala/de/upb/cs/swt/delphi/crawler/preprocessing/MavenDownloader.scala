7503b51af33082a38ec4971e4e56dd252db5fce1
Ben Hermann <ben.hermann@googlemail.com>
2018-06-17 16:50:41 +0200

    Removed unused inputs

--- src/main/scala/de/upb/cs/swt/delphi/crawler/preprocessing/MavenDownloader.scala
+++ src/main/scala/de/upb/cs/swt/delphi/crawler/preprocessing/MavenDownloader.scala
@@ -1,17 +1,8 @@
 package de.upb.cs.swt.delphi.crawler.preprocessing
 
-import java.io.InputStream
-
-import scala.concurrent.ExecutionContext.Implicits.global
 import java.net.URI
 
-import de.upb.cs.swt.delphi.crawler.BuildInfo
 import de.upb.cs.swt.delphi.crawler.discovery.maven.{HttpResourceHandler, MavenIdentifier}
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.impl.client.HttpClientBuilder
-
-import scala.concurrent.Future
-import scala.io.Source
 
 class MavenDownloader(identifier: MavenIdentifier) {
   val http = new HttpResourceHandler(constructArtifactBaseUri())
