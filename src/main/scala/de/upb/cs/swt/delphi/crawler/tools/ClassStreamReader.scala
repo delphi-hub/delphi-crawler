d3a6b851f2df1c60228497810734dbd91dd9fbdd
Ben Hermann <ben.hermann@googlemail.com>
2018-07-05 18:32:05 +0200

    Added OPAL Project creation from InputStreams

--- src/main/scala/de/upb/cs/swt/delphi/crawler/tools/ClassStreamReader.scala
+++ src/main/scala/de/upb/cs/swt/delphi/crawler/tools/ClassStreamReader.scala
@@ -1,17 +1,20 @@
 package de.upb.cs.swt.delphi.crawler.tools
 
-import java.io.InputStream
+import java.io.{FileInputStream, InputStream}
+import java.net.URL
 
 import org.opalj.br.ClassFile
 import org.opalj.br.analyses.Project
 
 /**
   * Reifies Java classes from a list of (name, inputStream) tuples.
+  *
   * @author Ben Hermann
   */
 trait ClassStreamReader {
   /**
     * Reifies Java classes from a list of (name, inputStream) tuples.
+    *
     * @param jarEntryStream A list of (name, inputStream) tuples.
     * @return A list of class files found in the stream.
     */
@@ -20,4 +23,17 @@ trait ClassStreamReader {
       .flatMap(e => Project.JavaLibraryClassFileReader.ClassFile(() => e._2))
   }
 
+  /**
+    * Creates a OPAL Project from a list of named InputStreams
+    * @param source The source of the JAR file
+    * @param jarEntryStream The converted list of InputStreams of classes with their entry names
+    * @return An OPAL Project including the JRE as library classes
+    */
+  def createProject(source: URL, jarEntryStream: Traversable[(String, InputStream)]): Project[URL] = {
+    val projectClasses: Traversable[(ClassFile, URL)] = reifyClasses(jarEntryStream).map(c => (c, source))
+    val libraryClasses: Traversable[(ClassFile, URL)] =
+      reifyClasses(new JarStreamReader(new FileInputStream(org.opalj.bytecode.RTJar)).readFully())
+        .map(c => (c, org.opalj.bytecode.RTJar.toURI.toURL))
+    Project(projectClasses, libraryClasses, true, Nil)
+  }
 }
--- src/test/scala/de/upb/cs/swt/delphi/crawler/tools/ClassStreamReaderTest.scala
+++ src/test/scala/de/upb/cs/swt/delphi/crawler/tools/ClassStreamReaderTest.scala
@@ -2,6 +2,7 @@ package de.upb.cs.swt.delphi.crawler.tools
 
 import java.io.BufferedInputStream
 
+import org.opalj.br.analyses.Project
 import org.scalatest.{FlatSpec, Matchers}
 
 import scala.reflect.io.File
@@ -18,5 +19,17 @@ class ClassStreamReaderTest extends FlatSpec with Matchers {
     val classes = new ClassStreamReader {}.reifyClasses(entries)
 
     classes.size should be (23)
+  }
+
+  "A ClassStreamReader" should "be able to produce a useful OPAL Project instance" in {
+    val file = File("src/test/resources/sootconfig-1.1.jar")
+    val fis = file.inputStream()
+    val jsr = new JarStreamReader(new BufferedInputStream(fis))
+    val entries = jsr.readFully()
+
+    val p = new ClassStreamReader {}.createProject(file.toURL, entries)
+
+    p shouldBe a [Project[_]]
+    p.projectClassFilesCount should be (23)
   }
 }
