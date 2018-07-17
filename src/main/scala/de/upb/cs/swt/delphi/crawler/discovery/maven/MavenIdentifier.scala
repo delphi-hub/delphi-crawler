856065de68457e8c3bebf4a3bd1e3e68d1cbe9b4
Ben Hermann <ben.hermann@googlemail.com>
2018-07-17 18:38:01 +0200

    Fixing uniqueness check with pre-existing artifacts

--- src/main/scala/de/upb/cs/swt/delphi/crawler/discovery/maven/MavenIdentifier.scala
+++ src/main/scala/de/upb/cs/swt/delphi/crawler/discovery/maven/MavenIdentifier.scala
@@ -2,4 +2,8 @@ package de.upb.cs.swt.delphi.crawler.discovery.maven
 
 import de.upb.cs.swt.delphi.crawler.Identifier
 
-case class MavenIdentifier(val repository: String, val groupId: String, val artifactId: String, val version: String) extends Identifier
+case class MavenIdentifier(val repository: String, val groupId: String, val artifactId: String, val version: String) extends Identifier {
+  def toUniqueString = {
+    repository + ":" + groupId + ":" + artifactId + ":" + version
+  }
+}
