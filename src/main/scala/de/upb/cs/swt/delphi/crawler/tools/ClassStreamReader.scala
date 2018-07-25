307b3c8624d9176bcdaa8c898deb65d4f8c4838d
Ben Hermann <ben.hermann@googlemail.com>
2018-07-25 15:52:16 +0200

    Implemented a faster and tamer version of OPAL project creation for HTTP-sourced JARs

--- src/main/scala/de/upb/cs/swt/delphi/crawler/tools/ClassStreamReader.scala
+++ src/main/scala/de/upb/cs/swt/delphi/crawler/tools/ClassStreamReader.scala
@@ -1,46 +1,74 @@
 package de.upb.cs.swt.delphi.crawler.tools
 
-import java.io.{FileInputStream, InputStream}
+import java.io._
 import java.net.URL
+import java.util.jar.{JarEntry, JarInputStream}
 
 import com.typesafe.config.Config
 import org.opalj.br.ClassFile
 import org.opalj.br.analyses.Project
 import org.opalj.log.{GlobalLogContext, OPALLogger}
 
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
+
 /**
-  * Reifies Java classes from a list of (name, inputStream) tuples.
+  * Adapts OPAL to allow reading from HTTP resources directly
   *
   * @author Ben Hermann
+  * @author Michael Eichberg
   */
 trait ClassStreamReader {
-  /**
-    * Reifies Java classes from a list of (name, inputStream) tuples.
-    *
-    * @param jarEntryStream A list of (name, inputStream) tuples.
-    * @return A list of class files found in the stream.
-    */
-  def reifyClasses(jarEntryStream: Traversable[(String, InputStream)]): Traversable[ClassFile] = {
-    val config : Config = org.opalj.br.BaseConfig
 
+  /**
+    * Reifies classes in a provided JAR from any origin
+    * @param in An input stream of a JAR file
+    * @return A list of named reified class files including bodies
+    */
+  def readClassFiles(in: ⇒ JarInputStream): List[(ClassFile, String)] = org.opalj.io.process(in) { in ⇒
+    val config : Config = org.opalj.br.BaseConfig
     val reader = Project.JavaClassFileReader(GlobalLogContext, config)
-    jarEntryStream.filter(e => e._1.endsWith(".class"))
-      .flatMap(e => reader.ClassFile(() => e._2))
+    var je: JarEntry = in.getNextJarEntry()
+
+    var futures: List[Future[List[(ClassFile, String)]]] = Nil
+
+    while (je != null) {
+      val entryName = je.getName
+      if (je.getSize.toInt > 0 && entryName.endsWith(".class")) {
+        val entryBytes = new Array[Byte](je.getSize.toInt)
+
+        var remaining = entryBytes.length
+        var offset = 0
+        while (remaining > 0) {
+          val readBytes = in.read(entryBytes, offset, remaining)
+          if (readBytes < 0) throw new EOFException()
+          remaining -= readBytes
+          offset += readBytes
+        }
+        futures ::= Future[List[(ClassFile, String)]] {
+          val cfs = reader.ClassFile(new DataInputStream(new ByteArrayInputStream(entryBytes)))
+          cfs map { cf ⇒ (cf, entryName) }
+        }(org.opalj.concurrent.OPALExecutionContext)
+      }
+      je = in.getNextJarEntry()
+    }
+
+    futures.flatMap(f ⇒ Await.result(f, Duration.Inf))
   }
 
   /**
-    * Creates a OPAL Project from a list of named InputStreams
+    * Creates a OPAL Project from a jar input stream
     * @param source The source of the JAR file
-    * @param jarEntryStream The converted list of InputStreams of classes with their entry names
+    * @param jarInputStream An input stream for the JAR file
     * @return An OPAL Project including the JRE as library classes
     */
-  def createProject(source: URL, jarEntryStream: Traversable[(String, InputStream)]): Project[URL] = {
+  def createProject(source: URL, jarInputStream: JarInputStream): Project[URL] = {
     val config : Config = org.opalj.br.BaseConfig
 
-    val projectClasses: Traversable[(ClassFile, URL)] = reifyClasses(jarEntryStream).map(c => (c, source))
+    val projectClasses: Traversable[(ClassFile, URL)] = readClassFiles(jarInputStream).map(c => (c._1, source))
     val libraryClasses: Traversable[(ClassFile, URL)] =
-      reifyClasses(new JarStreamReader(new FileInputStream(org.opalj.bytecode.RTJar)).readFully())
-        .map(c => (c, org.opalj.bytecode.RTJar.toURI.toURL))
+      readClassFiles(new JarInputStream(new FileInputStream(org.opalj.bytecode.RTJar)))
+        .map(c => (c._1, org.opalj.bytecode.RTJar.toURI.toURL))
 
 
     Project(projectClasses, libraryClasses, true, Traversable.empty)(config, OPALLogAdapter)
--- src/test/scala/de/upb/cs/swt/delphi/crawler/tools/ClassStreamReaderTest.scala
+++ src/test/scala/de/upb/cs/swt/delphi/crawler/tools/ClassStreamReaderTest.scala
@@ -1,33 +1,70 @@
 package de.upb.cs.swt.delphi.crawler.tools
 
 import java.io.BufferedInputStream
+import java.net.{HttpURLConnection, URL}
+import java.util.jar.JarInputStream
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives.{get, getFromFile, path}
+import akka.http.scaladsl.server.Route
+import akka.stream.ActorMaterializer
 import org.opalj.br.analyses.Project
-import org.scalatest.{FlatSpec, Matchers}
+import org.opalj.log.{GlobalLogContext, OPALLogger}
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
 
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration._
 import scala.reflect.io.File
+import scala.util.Random
 
 /**
   * @author Ben Hermann
   */
-class ClassStreamReaderTest extends FlatSpec with Matchers {
-  "A ClassStreamReader" should "be able to reify classes from sootconfig-1.1" in {
-    val fis = File("src/test/resources/sootconfig-1.1.jar").inputStream()
-    val jsr = new JarStreamReader(new BufferedInputStream(fis))
-    val entries = jsr.readFully()
+class ClassStreamReaderTest extends FlatSpec with Matchers with BeforeAndAfter {
 
-    val classes = new ClassStreamReader {}.reifyClasses(entries)
-
-    classes.size should be (23)
+  before {
+    OPALLogger.updateLogger(GlobalLogContext, OPALLogAdapter)
   }
 
   "A ClassStreamReader" should "be able to produce a useful OPAL Project instance" in {
     val file = File("src/test/resources/sootconfig-1.1.jar")
     val fis = file.inputStream()
-    val jsr = new JarStreamReader(new BufferedInputStream(fis))
-    val entries = jsr.readFully()
+    val jis = new JarInputStream(new BufferedInputStream(fis))
 
-    val p = new ClassStreamReader {}.createProject(file.toURL, entries)
+    val p = new ClassStreamReader {}.createProject(file.toURL, jis)
+
+    p shouldBe a [Project[_]]
+    p.projectClassFilesCount should be (23)
+    p.allMethodsWithBody.size should not be (0)
+  }
+
+  it should "be able to read from a HTTP resource" in {
+
+    val route: Route =
+      get {
+        path("sootconfig-1.1.jar") {
+          getFromFile("src/test/resources/sootconfig-1.1.jar")
+        }
+      }
+    implicit val system : ActorSystem = ActorSystem("delphi-crawler")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val ec : ExecutionContext = system.dispatcher
+
+    val port = Random.nextInt(10000) + 10000
+
+    val f = Http().bindAndHandle(Route.handlerFlow(route), "0.0.0.0", port )
+
+    Await.ready(Future { Thread.sleep(100); true}, 150.milliseconds)
+    val target = new URL(s"http://127.0.0.1:$port/sootconfig-1.1.jar")
+    val conn = target.openConnection.asInstanceOf[HttpURLConnection]
+    conn.setRequestMethod("GET")
+    conn.setRequestProperty("User-Agent", s"Delphi Maven-Indexer-Reader/1.0.0-SNAPSHOT")
+
+    val bufferedInputStream = new BufferedInputStream(conn.getInputStream)
+    val jis = new JarInputStream(bufferedInputStream)
+
+    val p = new ClassStreamReader {}.createProject(target, jis)
 
     p shouldBe a [Project[_]]
     p.projectClassFilesCount should be (23)
