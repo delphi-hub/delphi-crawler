9777aee16fd62538b293e9532410e0c2a622f290
Ben Hermann <ben.hermann@googlemail.com>
2018-08-14 14:49:22 +0200

    Refactoring for storage queries

--- src/main/scala/de/upb/cs/swt/delphi/crawler/storage/ElasticActor.scala
+++ src/main/scala/de/upb/cs/swt/delphi/crawler/storage/ElasticActor.scala
@@ -17,53 +17,29 @@
 package de.upb.cs.swt.delphi.crawler.storage
 
 import akka.actor.{Actor, ActorLogging, Props}
-import com.sksamuel.elastic4s.ElasticsearchClientUri
-import com.sksamuel.elastic4s.http.ElasticDsl._
-import com.sksamuel.elastic4s.http.{ElasticClient, HttpClient}
+import akka.event.LoggingAdapter
+import com.sksamuel.elastic4s.http.ElasticClient
 import de.upb.cs.swt.delphi.crawler.Identifier
 import de.upb.cs.swt.delphi.crawler.discovery.git.GitIdentifier
 import de.upb.cs.swt.delphi.crawler.discovery.maven.MavenIdentifier
 import de.upb.cs.swt.delphi.crawler.processing.HermesResults
 
-class ElasticActor(client: ElasticClient) extends Actor with ActorLogging with ArtifactIdentityQuery {
+/**
+  * An actor reacting to item which should be pushed to elasticsearch
+  * @param client The currently active elasticsearch client
+  * @author Ben Hermann
+  */
+class ElasticActor(client: ElasticClient) extends Actor with ActorLogging with ArtifactIdentityQuery with ElasticStoreQueries {
+  private implicit val c : ElasticClient = client
+  private implicit val l : LoggingAdapter = log
 
-  override def receive = {
-    case m : MavenIdentifier => {
-      log.info("Pushing new maven identifier to elastic: [{}]", m)
-      client.execute {
-        indexInto(delphiProjectType).fields( "name" -> m.toUniqueString,
-                                                     "source" -> "Maven",
-                                                     "identifier" -> Map(
-                                                       "groupId" -> m.groupId,
-                                                       "artifactId" -> m.artifactId,
-                                                       "version" -> m.version))
-      }.await
-    }
-    case g : GitIdentifier => {
-      log.info("Pushing new git identifier to elastic: [{}]", g)
-      client.execute {
-        indexInto(delphiProjectType).fields("name" -> (g.repoUrl +"/"+ g.commitId),
-                                                     "source" -> "Git",
-                                                     "identifier" -> Map(
-                                                       "repoUrl" -> g.repoUrl,
-                                                       "commitId" -> g.commitId))
-      }
-    }
-    case h : HermesResults => {
-
-      implicit val c = client
-      elasticId(h.identifier) match {
-        case Some(id) => {
-          log.info(s"Pushing Hermes results for ${h.identifier} under id $id.")
-          client.execute {
-            update(id).in(delphiProjectType).doc("features" -> h.featureMap)
-          }.await
-        }
-        case None => log.warning(s"Tried to push hermes results for non-existing identifier: ${h.identifier}.")
-      }
-    }
+  override def receive: PartialFunction[Any, Unit] = {
+    case m : MavenIdentifier => store(m)
+    case g : GitIdentifier => store(g)
+    case h : HermesResults => store(h)
     case x => log.warning("Received unknown message: [{}] ", x)
   }
+
 }
 
 object ElasticActor {
